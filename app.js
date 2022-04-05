91003b2632fe50b1f9f2abab8998964dee3bdc06
RamonFavero <ramon.favero@hotmail.com>
2022-04-05 14:57:13 -0300

    hiding the encrypt message
    added dotenv to hide the encrypt message and a .gitignore

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -6,6 +7,8 @@ const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
 
 const app = express();
+
+
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -19,9 +22,9 @@ const userSchema = new mongoose.Schema ( {
   email:String,
   password:String
 });
- //////// IF HACKER FIND 'secret' ALL ENCRYPTED PASSWORDS CAN BE DISCOVERED 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret:secret, encryptedFields:['password']})
+ //////// IF HACKER FIND 'secret' ALL ENCRYPTED PASSWORDS CAN BE DISCOVERED
+//// const secret MOVED TO .env FILE
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields:['password']})
 
 const User = new mongoose.model('User', userSchema);
 
